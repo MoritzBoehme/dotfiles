d0522cb15cb508ec6f7e2d02ed72ae231da72ad4
MoritzBoehme <mail@moritzboeh.me>
2021-09-08 20:58:46 +0200

    removed fetch from tarball

--- modules/default.nix
+++ modules/default.nix
@@ -2,7 +2,7 @@
 
 {
   # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
+  #programs.home-manager.enable = true;
 
   # paths it should manage.
   home.username = "moritz";
@@ -41,26 +41,8 @@
         cat = "bat";
       };
 
-      plugins = [
-                  {
-                    name = "zsh-autosuggestions";
-                    src = pkgs.fetchFromGitHub {
-                      owner = "zsh-users";
-                      repo = "zsh-autosuggestions";
-                      rev = "v0.4.0";
-                      sha256 = "0z6i9wjjklb4lvr7zjhbphibsyx51psv50gm07mbb0kj9058j6kc";
-                    };
-                  }
-                  {
-                    name = "zsh-syntax-highlighting";
-                    src = pkgs.fetchFromGitHub {
-                      owner = "zsh-users";
-                      repo = "zsh-syntax-highlighting";
-                      rev = "0e1bb14452e3fc66dcc81531212e1061e02c1a61";
-                      sha256 = "09ncmyqlk9a3h470z0wgbkrznb5zyc9dj96011wm89rdxc1irxk2";
-                    };
-                  }
-                ];
+      autosuggestions.enable = true;
+      syntaxHighlighting.enable = true;
     };
 
     kitty = {
