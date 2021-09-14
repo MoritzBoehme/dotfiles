f0aee80b8cb5478ac811ba7d5e39638753bfce04
MoritzBoehme <mail@moritzboeh.me>
2021-09-14 21:00:26 +0200

    fixed relative path

--- modules/services/agenix.nix
+++ modules/services/agenix.nix
@@ -5,9 +5,9 @@
   environment.systemPackages = [
     inputs.agenix.defaultPackage.x86_64-linux
   ];
-  age.secrets.nordvpn.file = ../secrets/nordvpn.age;
+  age.secrets.nordvpn.file = ../../secrets/nordvpn.age;
   age.secrets.davfs = {
-    file = ../secrets/davfs.age;
+    file = ../../secrets/davfs.age;
     mode = "600";
     path = "/etc/davfs2/secrets";
   };
