249be1b40fb212ba3994cd1b9dbd4554d507704c
MoritzBoehme <mail@moritzboeh.me>
2021-09-14 23:04:13 +0200

    added diskstation

--- modules/services/diskstation/default.nix
+++ modules/services/diskstation/default.nix
@@ -11,12 +11,16 @@
       let
         mapConf = pkgs.writeText "auto" ''
           keepass -fstype=davfs,uid=1000 :https\://davs.home.boehmies.de/home/Drive/
+          diskstation -fstype=davfs,uid=1000 :https\://192.168.0.2\:5006/home/Drive/
         '';
       in
         ''
           /auto file:${mapConf}
         '';
     debug = true;
+  };
+  environment.etc."davfs2/certs/diskstation.pem" = {
+    text = builtins.readFile ./diskstation.pem;
   };
 
   home-manager.users.moritz = {
