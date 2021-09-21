408b016b04fce6a45631a668d7136296b28e751e
MoritzBoehme <mail@moritzboeh.me>
2021-09-21 17:48:34 +0200

    added certificate

--- modules/services/diskstation/default.nix
+++ modules/services/diskstation/default.nix
@@ -3,38 +3,38 @@
 {
   services.davfs2 = {
     enable = true;
-    extraConfig = "trust_server_cert diskstation.pem";
+    extraConfig = ''
+      [/auto/diskstation]
+      trust_server_cert diskstation.pem
+      [/auto/keepass]
+      trust_server_cert home-boehmies-de.pem
+    '';
   };
   services.autofs = {
     enable = true;
-    autoMaster =
-      let
-        mapConf = pkgs.writeText "auto" ''
-          keepass -fstype=davfs,uid=1000 :https\://davs.home.boehmies.de/home/Drive/
-          diskstation -fstype=davfs,uid=1000 :https\://192.168.0.2\:5006/home/Drive/
-        '';
-      in
-      ''
-        /auto file:${mapConf}
+    autoMaster = let
+      mapConf = pkgs.writeText "auto" ''
+        keepass -fstype=davfs,uid=1000 :https\://davs.home.boehmies.de/home/Drive/
+        diskstation -fstype=davfs,uid=1000 :https\://192.168.0.2\:5006/home/Drive/
       '';
+    in ''
+      /auto file:${mapConf}
+    '';
     debug = true;
   };
   environment.etc."davfs2/certs/diskstation.pem" = {
     text = builtins.readFile ./diskstation.pem;
+  };
+  environment.etc."davfs2/certs/home-boehmies-de.pem" = {
+    text = builtins.readFile ./home-boehmies-de.pem;
   };
 
   home-manager.users.moritz = {
     services.unison = {
       enable = true;
       pairs = {
-        keepass.roots = [
-          "/home/moritz/Keepass"
-          "/auto/keepass"
-        ];
-        diskstation.roots = [
-          "/home/moritz/Documents"
-          "/auto/diskstation"
-        ];
+        keepass.roots = [ "/home/moritz/Keepass" "/auto/keepass" ];
+        diskstation.roots = [ "/home/moritz/Documents" "/auto/diskstation" ];
       };
     };
   };
