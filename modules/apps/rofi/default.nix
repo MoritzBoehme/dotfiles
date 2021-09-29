27279976b2cf9196f43a28cd842d4aae55d97e46
MoritzBoehme <mail@moritzboeh.me>
2021-09-29 13:34:38 +0200

    added rofi plugins/scripts

--- modules/apps/rofi/default.nix
+++ modules/apps/rofi/default.nix
@@ -4,11 +4,17 @@ let
   base = (home: {
     programs.rofi = {
       enable = true;
+      package = pkgs.rofi.override { plugins = with pkgs; [ rofi-calc ]; };
       # theme = ~/.dotfiles/config/rofi/dracula_old.rasi;
       theme = ./dracula.rasi;
     };
+    home.packages = with pkgs; [ networkmanager_dmenu ];
+    xdg = {
+      enable = true;
+      configFile."networkmanager-dmenu/config.ini".text = ''
+        [dmenu]
+        dmenu_command = rofi
+      '';
+    };
   });
-in
-{
-  home-manager.users.moritz = { ... }: (base "/home/moritz/");
-}
+in { home-manager.users.moritz = { ... }: (base "/home/moritz/"); }
