79fa032a1565ad547b5ce555ee7768e7a8b33b98
MoritzBoehme <mail@moritzboeh.me>
2021-09-30 19:41:08 +0200

    added security module

--- modules/default.nix
+++ modules/default.nix
@@ -1,33 +1,28 @@
 { lib, pkgs, ... }:
 
 {
-  imports = [
-    ./apps
-    ./cli
-    ./desktop
-    ./services
-  ];
+  imports = [ ./apps ./cli ./desktop ./services ./security.nix ];
 
   # USERS
   users.users.moritz = {
     shell = pkgs.zsh;
     isNormalUser = true;
     home = "/home/moritz";
-    extraGroups = [ "wheel" "networkmanager" "video" ]; # Enable ‘sudo’ for the user.
+    extraGroups =
+      [ "wheel" "networkmanager" "video" ]; # Enable ‘sudo’ for the user.
   };
 
-  fonts.fonts = with pkgs; [
-    (nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" "JetBrainsMono" ]; })
-  ];
+  fonts.fonts = with pkgs;
+    [
+      (nerdfonts.override {
+        fonts = [ "FiraCode" "DroidSansMono" "JetBrainsMono" ];
+      })
+    ];
 
   time.timeZone = "Europe/Berlin";
 
   # PACKAGES
-  environment.systemPackages = with pkgs; [
-    vim
-    wget
-    firefox
-  ];
+  environment.systemPackages = with pkgs; [ vim wget firefox ];
 
   home-manager.users.moritz = {
     # Let Home Manager install and manage itself.
