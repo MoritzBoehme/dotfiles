109bf3fb1b62c7f74d21625c8ebe1f25e1489d62
MoritzBoehme <mail@moritzboeh.me>
2021-10-03 16:42:48 +0200

    run containers on desktop instead of laptop

--- flake.nix
+++ flake.nix
@@ -66,8 +66,8 @@
         agenix.nixosModules.age
       ];
 
-      hosts.nixos-laptop.modules =
-        [ ./hosts/nixos-laptop ./modules/containers ];
-      hosts.nixos-desktop.modules = [ ./hosts/nixos-desktop ./modules/gaming ];
+      hosts.nixos-laptop.modules = [ ./hosts/nixos-laptop ];
+      hosts.nixos-desktop.modules =
+        [ ./hosts/nixos-desktop ./modules/gaming ./modules/containers ];
     };
 }
