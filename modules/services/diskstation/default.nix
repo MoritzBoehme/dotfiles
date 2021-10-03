eeabaa6954506883fcc7a25cac1c7a31beb642d1
MoritzBoehme <mail@moritzboeh.me>
2021-10-03 16:31:01 +0200

    added media mount

--- modules/services/diskstation/default.nix
+++ modules/services/diskstation/default.nix
@@ -4,7 +4,12 @@
   services.davfs2 = {
     enable = true;
     extraConfig = ''
+      buf_size 32
+      use_compression 1
+      table_size 4096
       [/auto/diskstation]
+      trust_server_cert diskstation.pem
+      [/auto/media]
       trust_server_cert diskstation.pem
       [/auto/keepass]
       trust_server_cert home-boehmies-de.pem
@@ -16,6 +21,7 @@
       mapConf = pkgs.writeText "auto" ''
         keepass -fstype=davfs,uid=1000 :https\://davs.home.boehmies.de/home/Drive/
         diskstation -fstype=davfs,uid=1000 :https\://192.168.0.2\:5006/home/Drive/
+        media -fstype=davfs,uid=1000 :https\://192.168.0.2\:5006/media
       '';
     in ''
       /auto file:${mapConf}
@@ -34,7 +40,18 @@
       enable = true;
       pairs = {
         keepass.roots = [ "/home/moritz/Keepass" "/auto/keepass" ];
-        diskstation.roots = [ "/home/moritz/Documents" "/auto/diskstation" ];
+        diskstation = {
+          roots = [ "/home/moritz/Documents" "/auto/diskstation" ];
+          commandOptions = {
+            auto = "true";
+            batch = "true";
+            log = "false";
+            repeat = "watch";
+            sshcmd = "\${pkgs.openssh}/bin/ssh";
+            ui = "text";
+            fastcheck = "true";
+          };
+        };
       };
     };
   };
