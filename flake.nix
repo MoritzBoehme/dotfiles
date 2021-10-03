ad1c367067745324f30a289a248e48a9164a51c4
MoritzBoehme <mail@moritzboeh.me>
2021-10-03 16:33:35 +0200

    updated lock

--- flake.nix
+++ flake.nix
@@ -4,9 +4,8 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/release-21.05";
     unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    utils.url =
-      "github:gytis-ivaskevicius/flake-utils-plus/release-1.2.0-without-deprecated-code";
     nur.url = "github:nix-community/NUR";
+    utils.url = "github:gytis-ivaskevicius/flake-utils-plus/1.3.0";
     agenix.url = "github:ryantm/agenix";
 
     emacs-overlay.url = "github:nix-community/emacs-overlay";
