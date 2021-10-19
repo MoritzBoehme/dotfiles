6b7b1bb52a0ccf0eb33637622bf3585a32363bc9
MoritzBoehme <mail@moritzboeh.me>
2021-10-19 11:58:20 +0200

    fixup! switched to unstable

--- modules/apps/default.nix
+++ modules/apps/default.nix
@@ -4,7 +4,7 @@
   imports = [ ./rofi ./kitty.nix ./zathura.nix ./email.nix ./spotify.nix ];
   home-manager.users.moritz.home.packages = with pkgs; [
     neofetch
-    unstable.keepassxc
+    keepassxc
     xfce.thunar
     xfce.xfconf
     xfce.tumbler
