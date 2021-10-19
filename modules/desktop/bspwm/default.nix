aa726e1ccb4c9f44a514be01964489283d0007a9
MoritzBoehme <mail@moritzboeh.me>
2021-10-19 11:12:40 +0200

    use homemanager bspwm and sxhkd for config

--- modules/desktop/bspwm/default.nix
+++ modules/desktop/bspwm/default.nix
@@ -22,21 +22,35 @@
   console.keyMap = "de";
 
   home-manager.users.moritz = {
-    xsession.windowManager.bspwm.enable = true;
+    xsession.windowManager.bspwm = {
+      enable = true;
+      rules = {
+        "Zathura" = { state = "tiled"; };
+        "Emacs" = { state = "tiled"; };
+        "feh" = { state = "floating"; };
+      };
+      settings = {
+        border_width = 2;
+        window_gap = 5;
+        borderless_monocle = true;
+        gapless_monocle = true;
+        focus_follows_pointer = true;
+      };
+      startupPrograms = [
+        "${pkgs.systemd}/bin/systemctl --user start polybar.service"
+        "feh --bg-fill ~/.config/wallpaper/dracula.png"
+      ];
+      extraConfig = builtins.readFile ./bspwmrc;
+
+    };
+    services.sxhkd = {
+      enable = true;
+      extraConfig = builtins.readFile ./sxhkdrc;
+    };
 
     xdg = {
       enable = true;
-      configFile = {
-        "bspwm/bspwmrc" = {
-          source = ./bspwmrc;
-          onChange = "bspc wm -r";
-        };
-        "sxhkd/sxhkdrc" = {
-          source = ./sxhkdrc;
-          onChange = "pkill -USR1 -x sxhkd";
-        };
-        "wallpaper/dracula.png" = { source = ./dracula.png; };
-      };
+      configFile = { "wallpaper/dracula.png" = { source = ./dracula.png; }; };
     };
     home.packages = with pkgs; [ feh playerctl pamixer brightnessctl ];
   };
