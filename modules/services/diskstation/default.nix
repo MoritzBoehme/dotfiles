33df9ff59142ccc6df06701b7aa08868adb7b1ad
MoritzBoehme <mail@moritzboeh.me>
2021-11-04 17:29:28 +0100

    comments out no longer needed mounts

--- modules/services/diskstation/default.nix
+++ modules/services/diskstation/default.nix
@@ -1,26 +1,26 @@
 { config, lib, pkgs, ... }:
 
 {
-  services.davfs2 = {
-    enable = true;
-    extraConfig = ''
-      [/auto/keepass]
-      trust_server_cert home-boehmies-de.pem
-    '';
-  };
-  services.autofs = {
-    enable = true;
-    autoMaster = let
-      mapConf = pkgs.writeText "auto" ''
-        keepass -fstype=davfs,uid=1000 :https\://davs.home.boehmies.de/home/Drive/
-      '';
-    in ''
-      /auto file:${mapConf}
-    '';
-  };
-  environment.etc."davfs2/certs/home-boehmies-de.pem" = {
-    text = builtins.readFile ./home-boehmies-de.pem;
-  };
+  # services.davfs2 = {
+  #   enable = true;
+  #   extraConfig = ''
+  #     [/auto/keepass]
+  #     trust_server_cert home-boehmies-de.pem
+  #   '';
+  # };
+  # services.autofs = {
+  #   enable = true;
+  #   autoMaster = let
+  #     mapConf = pkgs.writeText "auto" ''
+  #       keepass -fstype=davfs,uid=1000 :https\://davs.home.boehmies.de/home/Drive/
+  #     '';
+  #   in ''
+  #     /auto file:${mapConf}
+  #   '';
+  # };
+  # environment.etc."davfs2/certs/home-boehmies-de.pem" = {
+  #   text = builtins.readFile ./home-boehmies-de.pem;
+  # };
 
   fileSystems."/media/media" = {
     device = "//192.168.0.2/media";
@@ -35,18 +35,18 @@
     ];
   };
 
-  fileSystems."/media/diskstation" = {
-    device = "//192.168.0.2/home/Drive";
-    fsType = "cifs";
-    options = let
-      # this line prevents hanging on network split
-      automount_opts =
-        "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+  # fileSystems."/media/diskstation" = {
+  #   device = "//192.168.0.2/home/Drive";
+  #   fsType = "cifs";
+  #   options = let
+  #     # this line prevents hanging on network split
+  #     automount_opts =
+  #       "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
 
-    in [
-      "${automount_opts},credentials=/run/secrets/smbMoritz,uid=1000,gid=100"
-    ];
-  };
+  #   in [
+  #     "${automount_opts},credentials=/run/secrets/smbMoritz,uid=1000,gid=100"
+  #   ];
+  # };
 
   # home-manager.users.moritz = {
   #   services.unison = {
