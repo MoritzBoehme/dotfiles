3d3da153d8f19cca2aa9ef3c95dd91e05d432c49
MoritzBoehme <mail@moritzboeh.me>
2021-11-06 23:27:28 +0100

    adds github copilot

--- modules/apps/vscode.nix
+++ modules/apps/vscode.nix
@@ -4,13 +4,19 @@
   home-manager.users.moritz = {
     programs.vscode = {
       enable = true;
-      package = pkgs.vscodium-fhs;
-      extensions = with pkgs; [
-        vscode-extensions.vscodevim.vim
-        vscode-extensions.dracula-theme.theme-dracula
-        vscode-extensions.esbenp.prettier-vscode
-        vscode-extensions.pkief.material-icon-theme
-      ];
+      package = pkgs.vscode-fhs;
+      extensions = with pkgs;
+        [
+          vscode-extensions.vscodevim.vim
+          vscode-extensions.dracula-theme.theme-dracula
+          vscode-extensions.esbenp.prettier-vscode
+          vscode-extensions.pkief.material-icon-theme
+        ] ++ vscode-utils.extensionsFromVscodeMarketplace [{
+          name = "copilot";
+          publisher = "GitHub";
+          version = "1.7.3689";
+          sha256 = "16zrrymxfymc0039zf48vm22rxjs22mh9zkvkpg45grx2a2m19zh";
+        }];
     };
   };
 }
