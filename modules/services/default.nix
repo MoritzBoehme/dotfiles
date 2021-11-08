d45cdff79687415fa012e9422e25a7fac335d202
MoritzBoehme <mail@moritzboeh.me>
2021-11-08 14:28:35 +0100

    adds printing drivers

--- modules/services/default.nix
+++ modules/services/default.nix
@@ -7,6 +7,11 @@
 
   services = {
     printing.enable = true;
+    printing.drivers = with pkgs; [ epson-escpr2 epson-escpr ];
+    avahi = {
+      enable = true;
+      nssmdns = true;
+    };
     pipewire = {
       enable = true;
       alsa.enable = true;
