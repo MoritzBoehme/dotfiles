b2092cbf25883f15ed82cbd593e007c90c5fa97f
MoritzBoehme <mail@moritzboeh.me>
2021-12-04 11:45:01 +0100

    add other packages

--- modules/cli/default.nix
+++ modules/cli/default.nix
@@ -3,7 +3,18 @@
 {
   imports = [ ./adb.nix ./direnv.nix ./git.nix ./nix.nix ./zsh.nix ];
   home-manager.users.moritz = {
-    home.packages = with pkgs; [ p7zip ];
+    home.packages = with pkgs; [
+      # archives
+      p7zip
+
+      # file manager
+      ranger
+
+      trash-cli
+
+      # fetcher
+      neofetch
+    ];
     programs = {
       exa.enable = true;
       bat.enable = true;
