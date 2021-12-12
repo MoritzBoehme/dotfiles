85be271c1e60220c08e78dd618f280a2c67a1048
MoritzBoehme <mail@moritzboeh.me>
2021-12-12 12:19:54 +0100

    adds nextcloud

--- modules/desktop/apps/default.nix
+++ modules/desktop/apps/default.nix
@@ -2,19 +2,27 @@
 
 {
   imports = [ ./rofi ./kitty.nix ./zathura.nix ./spotify.nix ./firefox.nix ];
-  home-manager.users.moritz.home.packages = with pkgs; [
-    anki
-    keepassxc
-    libreoffice
-    neofetch
-    pavucontrol
-    signal-desktop
-    virtualboxWithExtpack
-    xfce.exo
-    xfce.thunar
-    xfce.tumbler
-    xfce.xfconf
-  ];
+  home-manager.users.moritz = {
+    services.nextcloud-client = {
+      enable = true;
+      startInBackground = true;
+    };
+    home.packages = with pkgs; [
+      anki
+      keepassxc
+      libreoffice
+      neofetch
+      pavucontrol
+      signal-desktop
+      virtualboxWithExtpack
+      xfce.exo
+      xfce.thunar
+      xfce.tumbler
+      xfce.xfconf
+    ];
+  };
+  virtualisation.virtualbox.host.enable = true;
+  users.extraGroups.vboxusers.members = [ "moritz" ];
   services.gvfs = {
     enable = true;
     package = lib.mkForce pkgs.gnome3.gvfs;
