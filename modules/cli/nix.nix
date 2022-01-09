a697be901f8f39edad041e533945e5b1ea600a73
MoritzBoehme <mail@moritzboeh.me>
2022-01-09 14:15:05 +0100

    :rocket: enables nix gc and optimise

--- modules/cli/nix.nix
+++ modules/cli/nix.nix
@@ -2,4 +2,9 @@
 
 {
   nix.package = pkgs.nixFlakes;
+  nix.gc = {
+    automatic = true;
+    options = "--max-freed $((32 * 1024**3)) --delete-older-than 14d";
+  };
+  nix.optimise.automatic = true;
 }
