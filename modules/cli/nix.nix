e24954bca1188a4fe9ac344d44e1ce7ad1cdf859
MoritzBoehme <mail@moritzboeh.me>
2022-01-21 22:05:21 +0100

    :broom: refactor

--- modules/cli/nix.nix
+++ modules/cli/nix.nix
@@ -1,10 +1,12 @@
 { config, lib, pkgs, ... }:
 
 {
-  nix.package = pkgs.nixFlakes;
-  nix.gc = {
-    automatic = true;
-    options = "--max-freed $((32 * 1024**3)) --delete-older-than 14d";
+  nix = {
+    package = pkgs.nix_2_4;
+    gc = {
+      automatic = true;
+      options = "--max-freed $((32 * 1024**3)) --delete-older-than 14d";
+    };
+    optimise.automatic = true;
   };
-  nix.optimise.automatic = true;
 }
