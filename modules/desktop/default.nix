fa7c3c118d5d6fe2b500e31b640dd4bf709a650d
MoritzBoehme <mail@moritzboeh.me>
2022-01-21 22:10:09 +0100

    :rocket: remap capslock

--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -3,4 +3,7 @@
 {
   imports = [ ./apps ./bspwm ./polybar ./gtk.nix ];
   home-manager.users.moritz = { services.unclutter.enable = true; };
+  # Remap capslock to esc and shift + capslock to capslock
+  services.xserver.xkbOptions =
+    "terminate:ctrl_alt_bksp,caps:escape_shifted_capslock";
 }
