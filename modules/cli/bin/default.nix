db363a3f57178dec62e45708eddb65c95b9f8d48
Moritz Böhme <mail@moritzboeh.me>
2022-02-07 14:20:56 +0100

    :rocket: add script for changing wallpaper

--- modules/cli/bin/default.nix
+++ modules/cli/bin/default.nix
@@ -3,11 +3,12 @@
 let
   cheat = import ./cheat.nix { inherit pkgs; };
   cycleSinks = import ./cycleSinks.nix { inherit pkgs; };
+  git-switch = import ./git-switch.nix { inherit pkgs; };
   protonge = import ./protonge.nix { inherit pkgs; };
+  randomWallpaper = import ./randomWallpaper.nix { inherit pkgs; };
   share = import ./share.nix { inherit pkgs; };
   sxhkdHelp = import ./sxhkdHelp.nix { inherit pkgs; };
-  git-switch = import ./git-switch.nix { inherit pkgs; };
 in {
   home-manager.users.moritz.home.packages =
-    [ cheat cycleSinks protonge share sxhkdHelp git-switch ];
+    [ cheat cycleSinks git-switch protonge randomWallpaper share sxhkdHelp ];
 }
