f1281736a8e660e086ee3b51e2b2249a6c8d3dfd
Moritz Böhme <mail@moritzboeh.me>
2022-02-11 22:25:07 +0100

    :sparkles: prettify

--- modules/editors/emacs.nix
+++ modules/editors/emacs.nix
@@ -4,20 +4,18 @@ let
   emacs = with pkgs;
     ((emacsPackagesNgGen emacsGcc).emacsWithPackages
       (epkgs: [ epkgs.vterm epkgs.emacsql-sqlite3 ]));
-in
-{
+in {
   fonts.fonts = [ pkgs.emacs-all-the-icons-fonts ];
 
   home-manager.users.moritz = {
     home.sessionPath = [ "/home/moritz/.config/emacs/bin/" ];
-    programs.emacs.enable = true;
-    programs.emacs.package = emacs;
     services.emacs.enable = true;
     services.emacs.package = emacs;
 
     home.packages = with pkgs; [
       ## Emacs itself
       binutils # native-comp needs 'as', provided by this
+      emacs
 
       ## Doom dependencies
       git
@@ -36,23 +34,30 @@ in
       hunspellDicts.en_GB-ize
       hunspellDicts.en_US
       hunspellDicts.de_DE
+
       # :checkers grammar
       languagetool
+
       # :tools lookup & :lang org +roam
       sqlite
       wordnet
       graphviz
+
       # :lang latex & :lang org (latex previews)
       texlive.combined.scheme-full
+
       # :lang nix
       nixfmt # for formating nix
+
       # :lang markdown
       pandoc
+
       # :app everywhere
       xdotool
       xorg.xwininfo
       xclip
       xorg.xprop
+
       # :lang python
       python-language-server
     ];
