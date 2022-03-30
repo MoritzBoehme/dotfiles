a786b3a9bdfc43f82e306d2a00da7402b371c964
Moritz Böhme <mail@moritzboeh.me>
2022-03-30 21:51:18 +0200

    :sparkles: unclutter

--- modules/cli/direnv.nix
+++ modules/cli/direnv.nix
@@ -3,7 +3,6 @@
 {
   home-manager.users.moritz.programs.direnv = {
     enable = true;
-    enableZshIntegration = true;
     nix-direnv.enable = true;
   };
 }
--- modules/cli/ssh.nix
+++ modules/cli/ssh.nix
@@ -2,6 +2,6 @@
 
 {
   users.users.moritz.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGoAqa2m7hIzZ2LS96Z+RCIlRvhBM/j7h27tMBCwMT+a moritz@nixos-laptop"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGoAqa2m7hIzZ2LS96Z+RCIlRvhBM/j7h27tMBCwMT+a"
   ];
 }
