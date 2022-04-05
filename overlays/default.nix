e2052019d91d47cfe2037eedea09a6352eb9e0f0
Moritz Böhme <mail@moritzboeh.me>
2022-04-05 11:09:57 +0200

    :broom: fish 3.3 has been merged into unstable

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,7 +2,6 @@
 
 final: prev: {
   python-dev = import ./python.nix final prev;
-  fish = final.master.fish;
   nix-flake-tools = inputs.nix-flake-tools.packages.${prev.system}.extension;
   master = import inputs.master {
     inherit (prev) system;
