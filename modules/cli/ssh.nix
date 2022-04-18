7f1d9cef9c3b032dce369e8162493e96d7289a05
Moritz Böhme <mail@moritzboeh.me>
2022-04-18 12:51:31 +0200

    :rocket: add other ssh keys

--- modules/cli/ssh.nix
+++ modules/cli/ssh.nix
@@ -2,6 +2,8 @@
 
 {
   users.users.moritz.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGoAqa2m7hIzZ2LS96Z+RCIlRvhBM/j7h27tMBCwMT+a"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGoAqa2m7hIzZ2LS96Z+RCIlRvhBM/j7h27tMBCwMT+a" # Moritz
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDhtwHDGAZshiQWKkCcPWV9tC83b+bKBgjDcjP/N2CKO" # Laptop
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKl8gMhwSf1NsP5gp14xbbyjqQLZzcHLb/XKRMoHdXgI" # Desktop
   ];
 }
