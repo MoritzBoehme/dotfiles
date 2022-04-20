61dfc58f0728fd2e45f976af0180a3435eb2370b
Moritz Böhme <mail@moritzboeh.me>
2022-04-20 19:46:18 +0200

    Revert ":broom: remove user keys"
    This reverts commit cb601451d004374b534cb6d4d6a2d02de565c1c6.

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,15 +1,19 @@
 let
+  moritz =
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGoAqa2m7hIzZ2LS96Z+RCIlRvhBM/j7h27tMBCwMT+a";
+  users = [ moritz ];
+
   nixos-laptop =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDhtwHDGAZshiQWKkCcPWV9tC83b+bKBgjDcjP/N2CKO";
   nixos-desktop =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKl8gMhwSf1NsP5gp14xbbyjqQLZzcHLb/XKRMoHdXgI";
   hosts = [ nixos-laptop nixos-desktop ];
 in {
-  "nordvpn.age".publicKeys = hosts;
-  "spotifyd.age".publicKeys = hosts;
-  "email-desktop.age".publicKeys = hosts;
-  "email-laptop.age".publicKeys = hosts;
-  "home-vpn.age".publicKeys = hosts;
-  "home-vpn-password.age".publicKeys = hosts;
-  "github.age".publicKeys = hosts;
+  "nordvpn.age".publicKeys = users ++ hosts;
+  "spotifyd.age".publicKeys = users ++ hosts;
+  "email-desktop.age".publicKeys = users ++ hosts;
+  "email-laptop.age".publicKeys = users ++ hosts;
+  "home-vpn.age".publicKeys = users ++ hosts;
+  "home-vpn-password.age".publicKeys = users ++ hosts;
+  "github.age".publicKeys = users ++ hosts;
 }
