fe31ea937f06a6744350ad9b33601bad322ab9b7
Moritz Böhme <mail@moritzboeh.me>
2022-06-27 17:57:42 +0200

    :broom: remove unused scripts

--- modules/cli/bin/cheat.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs }:
-
-pkgs.writeShellApplication {
-  name = "cheat";
-
-  runtimeInputs = with pkgs; [ curl bat ];
-
-  text = ''curl -s "cheat.sh/$1" | bat'';
-}
--- modules/cli/bin/default.nix
+++ modules/cli/bin/default.nix
@@ -1,21 +1,12 @@
 { config, lib, pkgs, ... }:
 
 let
-  cheat = import ./cheat.nix { inherit pkgs; };
   cycleSinks = import ./cycleSinks.nix { inherit pkgs; };
-  git-switch = import ./git-switch.nix { inherit pkgs; };
   protonge = import ./protonge.nix { inherit pkgs; };
   randomWallpaper = import ./randomWallpaper.nix { inherit pkgs; };
   share = import ./share.nix { inherit pkgs; };
   sxhkdHelp = import ./sxhkdHelp.nix { inherit pkgs; };
 in {
-  home-manager.users.moritz.home.packages = [
-    # cheat
-    cycleSinks
-    git-switch
-    protonge
-    randomWallpaper
-    share
-    sxhkdHelp
-  ];
+  home-manager.users.moritz.home.packages =
+    [ cycleSinks protonge randomWallpaper share sxhkdHelp ];
 }
